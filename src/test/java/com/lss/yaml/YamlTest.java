47dcfbf8853225f60f49e7f3e668b70c2284e2e8
ahhblss <909002538@qq.com>
2017-02-24 15:24:34 +0800

    init

---
+++ src/test/java/com/lss/yaml/YamlTest.java
@@ -0,0 +1,34 @@
+package com.lss.yaml;
+
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.esotericsoftware.yamlbeans.YamlReader;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ * Created by Sean.liu on 2017/2/24.
+ */
+public class YamlTest {
+
+    @Test
+    public void usageTest()
+    {
+        try {
+            YamlReader reader = new YamlReader(new InputStreamReader(
+                    this.getClass().getResourceAsStream("/contact.yml"), StandardCharsets.UTF_8.name()));
+            Object object = reader.read();
+            System.out.println(object);
+            Map map = (Map)object;
+            System.out.println(map.get("address"));
+            reader.close();
+        } catch (YamlException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
